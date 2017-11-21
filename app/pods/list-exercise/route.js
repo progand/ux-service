ca22ef4301a2f3c58607b701dcb75692b3c80f08
cfg111sleepy <ihorsleepy@gmail.com>
2017-11-21 21:27:43 +0200

    use fetch

--- app/pods/list-exercise/route.js
+++ app/pods/list-exercise/route.js
@@ -1,4 +1,11 @@
-import Route from '@ember/routing/route';
+import Ember from 'ember';
+import fetch from 'ember-fetch/ajax';
 
-export default Route.extend({
+export default Ember.Route.extend({
+  model () {
+    return fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(function(response) {
+        return response;
+      });
+  }
 });
