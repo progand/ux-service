d80377ccb366caeea75df9a508e7ee42b3e4f00d
progand <lach@ukr.net>
2017-12-09 15:46:24 +0200

    [*] Save age restrions in task

--- app/pods/tasks/new/route.js
+++ app/pods/tasks/new/route.js
@@ -2,11 +2,13 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   actions: {
-    submit(title, text) {
+    submit(title, text, minAge, maxAge) {
       if (title && text) {
         const task = this.store.createRecord('task', {
           title,
-          text
+          text,
+          minAge,
+          maxAge
         });
         task.save().then(() => this.transitionTo("tasks.index"));
       }
