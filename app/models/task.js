800dd488ed561ecb1591221daa5713e313cf1309
cfg111sleepy <ihorsleepy@gmail.com>
2017-12-09 15:09:17 +0200

    age

--- app/models/task.js
+++ app/models/task.js
@@ -2,5 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   title: DS.attr('string'),
-  text: DS.attr('string')
+  text: DS.attr('string'),
+  minAge: DS.attr('number'),
+  maxAge: DS.attr('number')
 });
