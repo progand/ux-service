3005b3c8657445962d6f18aa161bebdadd0142bb
progand <lach@ukr.net>
2017-12-11 07:30:01 +0200

    [*] Clear tarks/new controller on exet

--- app/pods/tasks/new/route.js
+++ app/pods/tasks/new/route.js
@@ -1,6 +1,15 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  resetController(controller, isExiting) {
+    if (isExiting) {
+      // isExiting would be false if only the route's model was changing
+      controller.set('title', '');
+      controller.set('minAge', 0);
+      controller.set('maxAge', 100);
+      controller.set('text', '');
+    }
+  },
   actions: {
     submit(title, text, minAge, maxAge) {
       if (title && text) {
