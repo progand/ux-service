3bb72b4bb839020f5284c2d39e75dc035e532619
progand <lach@ukr.net>
2018-01-05 18:20:35 +0200

    [*] Login page layout

--- app/pods/index/route.js
+++ app/pods/index/route.js
@@ -1,14 +1,7 @@
 import Route from '@ember/routing/route';
-import {
-  inject as service
-} from '@ember/service';
+import UnauthenticatedRouteMixin from 'ember-simple-auth/mixins/unauthenticated-route-mixin';
 
-export default Route.extend({
-  session: service(),
-
-  beforeModel() {
-    if (this.get('session.isAuthenticated')) {
-      this.transitionTo('tasks.index');
-    }
-  },
+export default Route.extend(UnauthenticatedRouteMixin, {
+  // redirect authenticated user to /tasks
+  routeIfAlreadyAuthenticated: 'tasks.index'
 });
