ca6218fb2ef1468cec8673edbffff67e9cef115b
progand <lach@ukr.net>
2018-01-05 18:14:09 +0200

    [*] Hide index page for authenticated user

--- app/pods/index/route.js
+++ app/pods/index/route.js
@@ -1,4 +1,14 @@
 import Route from '@ember/routing/route';
+import {
+  inject as service
+} from '@ember/service';
 
 export default Route.extend({
+  session: service(),
+
+  beforeModel() {
+    if (this.get('session.isAuthenticated')) {
+      this.transitionTo('tasks.index');
+    }
+  },
 });
