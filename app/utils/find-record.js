f272650151bb9b7a8e379b4584015d26dccbaade
progand <lach@ukr.net>
2018-01-09 10:56:07 +0200

    [-] Fixed `findRecord` util

--- app/utils/find-record.js
+++ app/utils/find-record.js
@@ -1,8 +1,8 @@
 export default async function findRecord(store, modelName, propertyName, propertyValue) {
   const records = await store.query(modelName, {
     orderBy: propertyName,
-    startAt: propertyValue,
-    limitToFirst: 1
+    endAt: propertyValue,
+    limitToLast: 1
   });
   return records.get('firstObject');
 }
