71be5810a00f1df6b41b3b5ff1860b7174382563
progand <lach@ukr.net>
2018-01-10 10:16:26 +0200

    [*] Sign in refractored with `ember-concurrency`

--- app/pods/login/controller.js
+++ app/pods/login/controller.js
@@ -2,20 +2,14 @@ import Controller from '@ember/controller';
 import {
   inject as service
 } from '@ember/service';
+import {
+  task
+} from 'ember-concurrency';
 
 export default Controller.extend({
   session: service(),
-  error: null,
 
-  actions: {
-    // action to trigger authentication with Google+
-    async authenticateWith(provider) {
-      try {
-        this.set('error', null);
-        await this.get('session').authenticate('authenticator:custom', this.get('store'), provider);
-      } catch (err) {
-        this.set('error', err);
-      }
-    }
-  }
+  authenticateWithProvider: task(function*(provider) {
+    return yield this.get('session').authenticate('authenticator:custom', this.get('store'), provider);
+  }).drop()
 });
