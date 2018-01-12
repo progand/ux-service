c327ddd896ffa01c3e2ce722695c2fe83b6abe01
progand <lach@ukr.net>
2018-01-12 10:37:52 +0200

    [*] Destroy not saved when leave route

--- app/pods/tasks/new/route.js
+++ app/pods/tasks/new/route.js
@@ -1,10 +1,19 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  async model() {
+  model() {
     return this.get('store').createRecord('task', {
       countries: [],
       interests: []
     });
+  },
+
+  actions: {
+    willTransition() {
+      const model = this.controller.get('model');
+      if (model.get('isNew')) {
+        model.destroyRecord();
+      }
+    }
   }
 });
