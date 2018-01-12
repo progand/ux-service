f1f8bd430c40169f7b3249169398a363547e7cd2
progand <lach@ukr.net>
2018-01-12 12:53:47 +0200

    [+] Ask user for fillout profile

--- app/pods/login/controller.js
+++ app/pods/login/controller.js
@@ -10,6 +10,11 @@ export default Controller.extend({
   session: service(),
 
   authenticateWithProvider: task(function*(provider) {
-    return yield this.get('session').authenticate('authenticator:custom', this.get('store'), provider);
+    yield this.get('session').authenticate('authenticator:custom', this.get('store'), provider);
+    const uid = this.get('session.data.authenticated.uid');
+    const user = this.get('store').peekAll('user').findBy('uid', uid);
+    if(!user.get('validations.isValid')){
+      this.transitionToRoute('me');
+    }
   }).drop()
 });
